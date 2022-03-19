f4846214e2f1015250f789b79320833d37490b11
bankole175 <bankolesamuel175@gmail.com>
2022-03-19 03:03:19 +0100

    create design framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="d-flex justify-content-center">
+            <div className="card shadow-l filter-card mt-3">
+                <div className="d-flex">
+                    <input type="text" className="form-control border-0" placeholder="Search with title"/>
+                </div>
+            </div>
+        </div>
+        <div className="row result justify-content-center">
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+        </div>
+        <div className="row mt-3 justify-content-center">
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+            <div className="col-md-3">
+                <div className="card output-card"></div>
+            </div>
+        </div>
+        <div className="clip-path"></div>
     </div>
   );
 }
